1d6276b47ec8e4f98a2c95389e7a75d75f83fb8c
sgajaejung <sgajaejung@gmail.com>
2014-04-26 17:03:45 +0900

    display tree

---
+++ Lecture11/DisplayTree/DisplayTree/DisplayTree.cpp
@@ -0,0 +1,289 @@
+// DisplayTree.cpp : 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "DisplayTree.h"
+#include <objidl.h>
+#include <gdiplus.h> 
+using namespace Gdiplus;
+#pragma comment( lib, "gdiplus.lib" ) 
+
+#include <vector>
+#include <string>
+using namespace std;
+
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;								// 현재 인스턴스입니다.
+TCHAR szTitle[MAX_LOADSTRING];					// 제목 표시줄 텍스트입니다.
+TCHAR szWindowClass[MAX_LOADSTRING];			// 기본 창 클래스 이름입니다.
+
+
+// Gdiplus 용 
+GdiplusStartupInput g_gdiplusStartupInput; 
+ULONG_PTR g_gdiplusToken;
+Graphics *g_graphics;
+Pen *g_pen; // 펜 객체.
+Brush *g_brush; // 브러쉬 객체.
+
+
+
+// UI 하나를 나타내는 구조체.
+struct sNode
+{
+	sNode() {}
+	sNode(int type0, const wstring &_name, const Rect &r, sNode *_child1=NULL, sNode *_child2=NULL) :
+	type(type0), name(_name), rect(r), child1(_child1), child2(_child2)
+	{
+	}
+
+	int type; // 노드 타입
+	wstring name; // 노드 이름
+	Rect rect; // 노드 위치 (상대좌표)
+	
+	sNode *child1; // 자식 노드1
+	sNode *child2; // 자식 노드2
+};
+
+sNode *g_root = NULL;
+
+
+
+// 이 코드 모듈에 들어 있는 함수의 정방향 선언입니다.
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+
+
+// Gdiplus 초기화.
+void InitGdiPlus(HWND hWnd)
+{
+	//Start Gdiplus 
+	Gdiplus::GdiplusStartup(&g_gdiplusToken, &g_gdiplusStartupInput, NULL); 
+	g_graphics = new Graphics(hWnd);
+	g_pen = new Pen(Color::Red);
+	g_brush = new SolidBrush(Color::White);
+}
+
+// Gdiplus 제거.
+void ReleaseGdiPlus()
+{
+	delete g_pen;
+	delete g_brush;
+	delete g_graphics;
+	// Shutdown Gdiplus 
+	Gdiplus::GdiplusShutdown(g_gdiplusToken); 
+}
+
+
+// UI Node 출력.
+void Display(sNode *parent, sNode *node, const Point &offset = Point(0,0))
+{
+	if (!node)
+		return;
+
+	Rect r = node->rect;
+	r.Offset(offset);
+	g_graphics->DrawRectangle(g_pen, r);
+
+	// Initialize arguments.
+	Font myFont(L"Arial", 16);
+	StringFormat format;
+	format.SetAlignment(StringAlignmentCenter);
+	SolidBrush blackBrush(Color(255, 0, 0, 0));
+
+	// Draw string.
+	g_graphics->DrawString( node->name.c_str(), -1, &myFont,
+		RectF((REAL)r.X, (REAL)r.Y, (REAL)r.Width, (REAL)r.Height), &format, &blackBrush);
+
+	// 자식 노드 출력.
+	Display(node, node->child1, Point(r.X, r.Y));
+	Display(node, node->child2, Point(r.X, r.Y));
+}
+
+
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: 여기에 코드를 입력합니다.
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// 전역 문자열을 초기화합니다.
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_DISPLAYTREE, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// 응용 프로그램 초기화를 수행합니다.
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DISPLAYTREE));
+
+	// 기본 메시지 루프입니다.
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	ReleaseGdiPlus();
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  목적: 창 클래스를 등록합니다.
+//
+//  설명:
+//
+//    Windows 95에서 추가된 'RegisterClassEx' 함수보다 먼저
+//    해당 코드가 Win32 시스템과 호환되도록
+//    하려는 경우에만 이 함수를 사용합니다. 이 함수를 호출해야
+//    해당 응용 프로그램에 연결된
+//    '올바른 형식의' 작은 아이콘을 가져올 수 있습니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DISPLAYTREE));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_DISPLAYTREE);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   목적: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   설명:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   InitGdiPlus(hWnd);
+
+   // UI Node 생성.
+   g_root = new sNode(0, L"Window", Rect(100,100,200,150),
+	   new sNode(1, L"Btn", Rect(10, 50, 100, 50))
+	   );
+
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  목적: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND	- 응용 프로그램 메뉴를 처리합니다.
+//  WM_PAINT	- 주 창을 그립니다.
+//  WM_DESTROY	- 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// 메뉴 선택을 구문 분석합니다.
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		Display(NULL, g_root, Point(100,100));
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
